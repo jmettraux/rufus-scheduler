f667d4ef58c84005e00f16e52d25ae37b5f54309
Justin Fitzsimmons <justinf@nulogy.com>
2011-09-21 10:43:52 -0400

    Fixes an indentation error in the default exception handler

--- lib/rufus/exception_handlers/default.rb
+++ lib/rufus/exception_handlers/default.rb
@@ -1,12 +1,12 @@
 module Rufus::ExceptionHandlers
   class Default
-   def handle_exception(job, exception)
-     puts '=' * 80
-     puts "scheduler caught exception :"
-     puts exception
-     exception.backtrace.each { |l| puts l }
-     puts '=' * 80
-   end
+    def handle_exception(job, exception)
+      puts '=' * 80
+      puts "scheduler caught exception :"
+      puts exception
+      exception.backtrace.each { |l| puts l }
+      puts '=' * 80
+    end
   end
 end
 
