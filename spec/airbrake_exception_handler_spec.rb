ea21f583b7af47058cf9e22394f4d6da350b2ad8
Justin Fitzsimmons <justinf@nulogy.com>
2011-09-21 15:20:09 -0400

    Hoptoad spec now swallows exception output

--- spec/airbrake_exception_handler_spec.rb
+++ spec/airbrake_exception_handler_spec.rb
@@ -12,8 +12,14 @@ describe Rufus::ExceptionHandlers::Airbrake do
   end
 
   it 'notifies airbrake about exceptions' do
+    require 'stringio' unless defined?(StringIO) # ruby 1.9
+
     error = RuntimeError.new('Houston we have a problem')
     ::Airbrake.should_receive(:notify).with(error)
+
+    stdout = $stdout
+    s = StringIO.new
+    $stdout = s
 
     @s.in 0.400 do
       raise error
@@ -21,6 +27,9 @@ describe Rufus::ExceptionHandlers::Airbrake do
 
     sleep 0.500
     sleep 0.500
+
+    $stdout = stdout
+    s.close
 
   end
 end
