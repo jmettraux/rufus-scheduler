0b045552d75c79fa8a922b436e0e9af2112d4da3
John Mettraux <jmettraux@gmail.com>
2012-10-12 13:34:10 +0900

    switch to 2012 for time too

--- lib/rufus/sc/rtime.rb
+++ lib/rufus/sc/rtime.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2005-2011, John Mettraux, jmettraux@gmail.com
+# Copyright (c) 2005-2012, John Mettraux, jmettraux@gmail.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
