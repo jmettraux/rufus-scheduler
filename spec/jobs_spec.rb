72fe9d671215e4a930d0128d1fc33c645df37242
John Mettraux <jmettraux@gmail.com>
2013-09-18 07:37:15 +0900

    add specs from CronJob (zero second and first_at)

--- spec/jobs_spec.rb
+++ spec/jobs_spec.rb
@@ -329,17 +329,56 @@ describe Rufus::Scheduler::Job do
   end
 
   describe Rufus::Scheduler::EveryJob do
-
-    #describe '#unschedule' do
-    #  it 'unschedules the job'
-    #end
   end
 
   describe Rufus::Scheduler::CronJob do
 
-    #describe '#unschedule' do
-    #  it 'unschedules the job'
+    context 'normal' do
+
+      it 'triggers near the zero second' do
+
+        job = @scheduler.schedule_cron '* * * * *' do; end
+
+        sleep_until_next_minute
+
+        (job.last_time.to_i % 10).should == 0
+      end
+    end
+
+    #context 'sub-minute' do
+    #
+    #  it 'triggers near the zero second' do
+    #
+    #    job = @scheduler.schedule_cron '* * * * * *' do; end
+    #
+    #    sleep 1.5
+    #
+    #    p job.last_time
+    #    p job.last_time.to_f
+    #  end
     #end
+
+    context 'first_at/in' do
+
+      it 'does not trigger before first_at is reached' do
+
+        t = Time.now
+
+        job =
+          @scheduler.schedule_cron '* * * * * *', :first_in => '3s' do
+            triggered = Time.now
+          end
+
+        sleep 1
+
+        #p [ t, t.to_f ]
+        #p [ job.last_time, job.last_time.to_f ]
+        #p [ job.first_at, job.first_at.to_f ]
+
+        job.first_at.should be_within_1s_of(t + 3)
+        job.last_time.should == nil
+      end
+    end
   end
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,8 +7,10 @@
 
 Thread.abort_on_exception = true
 
+
 require 'stringio'
 require 'rufus-scheduler'
+
 
 def local(*args)
   Time.local(*args)
@@ -17,6 +19,35 @@ alias lo local
 
 def utc(*args)
   Time.utc(*args)
+end
+
+def sleep_until_next_minute
+  min = Time.now.min
+  while Time.now.min == min; sleep 2; end
+end
+
+
+#require 'rspec/expectations'
+
+RSpec::Matchers.define :be_within_1s_of do |expected|
+
+  match do |actual|
+
+    if actual.respond_to?(:asctime)
+      (actual.to_f - expected.to_f).abs <= 1.0
+    else
+      false
+    end
+  end
+
+  failure_message_for_should do |actual|
+
+    if actual.respond_to?(:asctime)
+      "expected #{actual.inspect} to be within 1 second of #{expected}"
+    else
+      "expected Time instance, got a #{actual.inspect}"
+    end
+  end
 end
 
 
