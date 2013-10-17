8b64ad670c300c2686021d568c4da5814f0b24cc
John Mettraux <jmettraux@gmail.com>
2013-10-18 08:11:24 +0900

    fight gh-84 add more details

--- misc/gacha_test.rb
+++ misc/gacha_test.rb
@@ -7,8 +7,12 @@
 
 #
 # Linux 3.2.0-4-amd64 #1 SMP Debian 3.2.46-1+deb7u1 x86_64 GNU/Linux
+#
+#
+# * max work threads default to 35
+#
 # ruby 2.0.0p247 (2013-06-27 revision 41674) [x86_64-linux]
-# dies ("Killed") after reaching 22 work threads (max is the 35 default)
+# dies ("Killed") after reaching 22 work threads
 #
 # ruby 1.9.3p392 (2013-02-22 revision 39386) [x86_64-linux]
 # is fine, is pegged at 35
@@ -20,14 +24,26 @@
 # ruby 1.8.7 (2012-10-12 patchlevel 371) [x86_64-linux]
 # is fine, is pegged at 35
 #
+# ruby 2.0.0p195 (2013-05-14 revision 40734) [x86_64-linux
+# dies ("Killed") after reaching 21 work threads
+#
+#
+# * max work threads set at 7
+#
+# ruby 2.0.0p247 (2013-06-27 revision 41674) [x86_64-linux]
+# is pegged at 7, but dies ("Killed") after a while (135 seconds)
+#
+# ruby 2.0.0p195 (2013-05-14 revision 40734) [x86_64-linux
+# is pegged at 7, seems OK (stopped the test after 349 seconds)
+#
 
 puts "Ruby #{RUBY_VERSION}p#{RUBY_PATCHLEVEL}"
 
 
 require 'rufus-scheduler'
 
-s = Rufus::Scheduler.new
-#s = Rufus::Scheduler.new(:max_work_threads => 7)
+#s = Rufus::Scheduler.new
+s = Rufus::Scheduler.new(:max_work_threads => 7)
 
 s.every '5s', :overlap => false do
   puts '.. 1st task started'
@@ -49,7 +65,8 @@ loop do
   p [
     :elapsed, (Time.now - start).to_i,
     :threads, Thread.list.size,
-    :work_threads, s.work_threads.size
+    :work_threads, s.work_threads.size,
+    :active_work_threads, s.work_threads(:active).size
   ]
   sleep 1
 end
