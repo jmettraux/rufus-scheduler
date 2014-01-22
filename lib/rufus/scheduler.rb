23b4741eddb4e1a69a06059e8a9c2db4b2e0bdfb
John Mettraux <jmettraux@gmail.com>
2014-01-22 22:22:30 +0900

    3.0.5 pre

--- lib/rufus/scheduler.rb
+++ lib/rufus/scheduler.rb
@@ -38,7 +38,7 @@ module Rufus
     require 'rufus/scheduler/cronline'
     require 'rufus/scheduler/job_array'
 
-    VERSION = '3.0.4'
+    VERSION = '3.0.5'
 
     #
     # A common error class for rufus-scheduler
