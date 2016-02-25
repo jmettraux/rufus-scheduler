aae2321692b5ad40ef2ba5bedcf8a0b861e84fd2
John Mettraux <jmettraux@gmail.com>
2016-02-25 09:26:29 +0900

    3.2.1 pre

--- lib/rufus/scheduler.rb
+++ lib/rufus/scheduler.rb
@@ -39,7 +39,7 @@ module Rufus
     require 'rufus/scheduler/job_array'
     require 'rufus/scheduler/locks'
 
-    VERSION = '3.2.0'
+    VERSION = '3.2.1'
 
     #
     # A common error class for rufus-scheduler
