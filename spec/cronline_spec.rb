87c358858f9760c61b1ee84f57fee70e101b2042
John Mettraux <jmettraux@gmail.com>
2016-11-18 06:25:42 +0900

    Adapt CronLine spec to explict @timezone, gh-220
    Beforehand, the local timezone in the CronLine was stored as `nil`, from gh-220 on, it is stored as a TZInfo::Timezone (like CronLine with non-local timezones).

--- spec/cronline_spec.rb
+++ spec/cronline_spec.rb
@@ -48,60 +48,65 @@ describe Rufus::Scheduler::CronLine do
   def no_match(line, time)
     expect(cl(line).matches?(time)).to eq(false)
   end
-  def to_a(line, array)
-    expect(cla(line)).to eq(array)
+
+  def a_eq(line, array)
+    if array.last.is_a?(String) # tz
+      expect(cla(line)).to eq(array)
+    else
+      expect(cla(line)[0..-2]).to eq(array)
+    end
   end
 
   describe '.new' do
 
     it 'interprets cron strings correctly' do
 
-      to_a '* * * * *', [ [0], nil, nil, nil, nil, nil, nil, nil ]
-      to_a '10-12 * * * *', [ [0], [10, 11, 12], nil, nil,  nil, nil, nil, nil ]
-      to_a '* * * * sun,mon', [ [0], nil, nil, nil, nil, [0, 1], nil, nil ]
-      to_a '* * * * mon-wed', [ [0], nil, nil, nil, nil, [1, 2, 3], nil, nil ]
-      to_a '* * * * 7', [ [0], nil, nil, nil, nil, [0], nil, nil ]
-      to_a '* * * * 0', [ [0], nil, nil, nil, nil, [0], nil, nil ]
-      to_a '* * * * 0,1', [ [0], nil, nil, nil, nil, [0,1], nil, nil ]
-      to_a '* * * * 7,1', [ [0], nil, nil, nil, nil, [0,1], nil, nil ]
-      to_a '* * * * 7,0', [ [0], nil, nil, nil, nil, [0], nil, nil ]
-      to_a '* * * * sun,2-4', [ [0], nil, nil, nil, nil, [0, 2, 3, 4], nil, nil ]
+      a_eq '* * * * *', [ [0], nil, nil, nil, nil, nil, nil ]
+      a_eq '10-12 * * * *', [ [0], [10, 11, 12], nil, nil, nil, nil, nil ]
+      a_eq '* * * * sun,mon', [ [0], nil, nil, nil, nil, [0, 1], nil ]
+      a_eq '* * * * mon-wed', [ [0], nil, nil, nil, nil, [1, 2, 3], nil ]
+      a_eq '* * * * 7', [ [0], nil, nil, nil, nil, [0], nil ]
+      a_eq '* * * * 0', [ [0], nil, nil, nil, nil, [0], nil ]
+      a_eq '* * * * 0,1', [ [0], nil, nil, nil, nil, [0,1], nil ]
+      a_eq '* * * * 7,1', [ [0], nil, nil, nil, nil, [0,1], nil ]
+      a_eq '* * * * 7,0', [ [0], nil, nil, nil, nil, [0], nil ]
+      a_eq '* * * * sun,2-4', [ [0], nil, nil, nil, nil, [0, 2, 3, 4], nil ]
 
-      to_a '* * * * sun,mon-tue', [ [0], nil, nil, nil, nil, [0, 1, 2], nil, nil ]
+      a_eq '* * * * sun,mon-tue', [ [0], nil, nil, nil, nil, [0, 1, 2], nil ]
 
-      to_a '* * * * * *', [ nil, nil, nil, nil, nil, nil, nil, nil ]
-      to_a '1 * * * * *', [ [1], nil, nil, nil, nil, nil, nil, nil ]
-      to_a '7 10-12 * * * *', [ [7], [10, 11, 12], nil, nil, nil, nil, nil, nil ]
-      to_a '1-5 * * * * *', [ [1,2,3,4,5], nil, nil, nil, nil, nil, nil, nil ]
+      a_eq '* * * * * *', [ nil, nil, nil, nil, nil, nil, nil ]
+      a_eq '1 * * * * *', [ [1], nil, nil, nil, nil, nil, nil ]
+      a_eq '7 10-12 * * * *', [ [7], [10, 11, 12], nil, nil, nil, nil, nil ]
+      a_eq '1-5 * * * * *', [ [1,2,3,4,5], nil, nil, nil, nil, nil, nil ]
 
-      to_a '0 0 1 1 *', [ [0], [0], [0], [1], [1], nil, nil, nil ]
+      a_eq '0 0 1 1 *', [ [0], [0], [0], [1], [1], nil, nil ]
 
-      to_a '52 0 * * *', [ [0], [52], [0], nil, nil, nil, nil, nil ]
+      a_eq '52 0 * * *', [ [0], [52], [0], nil, nil, nil, nil ]
 
       #if ruby18?
-      #  to_a '0 23-24 * * *', [ [0], [0], [0, 23], nil, nil, nil, nil, nil ]
+      #  a_eq '0 23-24 * * *', [ [0], [0], [0, 23], nil, nil, nil, nil ]
       #else
-      #  to_a '0 23-24 * * *', [ [0], [0], [23, 0], nil, nil, nil, nil, nil ]
+      #  a_eq '0 23-24 * * *', [ [0], [0], [23, 0], nil, nil, nil, nil ]
       #end
         #
         # as reported by Aimee Rose in
         # https://github.com/jmettraux/rufus-scheduler/issues/56
-      to_a '0 23-24 * * *', [ [0], [0], [0, 23], nil, nil, nil, nil, nil ]
+      a_eq '0 23-24 * * *', [ [0], [0], [0, 23], nil, nil, nil, nil ]
 
       #if ruby18?
-      #  to_a '0 23-2 * * *', [ [0], [0], [0, 1, 2, 23], nil, nil, nil, nil, nil ]
+      #  a_eq '0 23-2 * * *', [ [0], [0], [0, 1, 2, 23], nil, nil, nil, nil ]
       #else
-      #  to_a '0 23-2 * * *', [ [0], [0], [23, 0, 1, 2], nil, nil, nil, nil, nil ]
+      #  a_eq '0 23-2 * * *', [ [0], [0], [23, 0, 1, 2], nil, nil, nil, nil ]
       #end
-      to_a '0 23-2 * * *', [ [0], [0], [0, 1, 2, 23], nil, nil, nil, nil, nil ]
+      a_eq '0 23-2 * * *', [ [0], [0], [0, 1, 2, 23], nil, nil, nil, nil ]
 
       # modulo forms work for five-field forms
-      to_a '*/17 * * * *', [[0], [0, 17, 34, 51], nil, nil, nil, nil, nil, nil]
-      to_a '13 */17 * * *', [[0], [13], [0, 17], nil, nil, nil, nil, nil]
+      a_eq '*/17 * * * *', [[0], [0, 17, 34, 51], nil, nil, nil, nil, nil]
+      a_eq '13 */17 * * *', [[0], [13], [0, 17], nil, nil, nil, nil]
 
       # modulo forms work for six-field forms
-      to_a '*/17 * * * * *', [[0, 17, 34, 51], nil, nil, nil, nil, nil, nil, nil]
-      to_a '13 */17 * * * *', [[13], [0, 17, 34, 51], nil, nil, nil, nil, nil, nil]
+      a_eq '*/17 * * * * *', [[0, 17, 34, 51], nil, nil, nil, nil, nil, nil]
+      a_eq '13 */17 * * * *', [[13], [0, 17, 34, 51], nil, nil, nil, nil, nil]
     end
 
     it 'rejects invalid weekday expressions' do
@@ -121,13 +126,13 @@ describe Rufus::Scheduler::CronLine do
 
     it 'interprets cron strings with TZ correctly' do
 
-      to_a('* * * * * EST', [ [0], nil, nil, nil, nil, nil, nil, 'EST' ])
-      to_a('* * * * * * EST', [ nil, nil, nil, nil, nil, nil, nil, 'EST' ])
+      a_eq('* * * * * EST', [ [0], nil, nil, nil, nil, nil, nil, 'EST' ])
+      a_eq('* * * * * * EST', [ nil, nil, nil, nil, nil, nil, nil, 'EST' ])
 
-      to_a(
+      a_eq(
         '* * * * * * America/Chicago',
         [ nil, nil, nil, nil, nil, nil, nil, 'America/Chicago' ])
-      to_a(
+      a_eq(
         '* * * * * * America/New_York',
         [ nil, nil, nil, nil, nil, nil, nil, 'America/New_York' ])
 
@@ -137,22 +142,22 @@ describe Rufus::Scheduler::CronLine do
 
     it 'interprets cron strings with / (slashes) correctly' do
 
-      to_a(
+      a_eq(
         '0 */2 * * *',
-        [ [0], [0], (0..23).select { |e| e.even? }, nil, nil, nil, nil, nil ])
-      to_a(
+        [ [0], [0], (0..23).select { |e| e.even? }, nil, nil, nil, nil ])
+      a_eq(
         '0 0-23/2 * * *',
-        [ [0], [0], (0..23).select { |e| e.even? }, nil, nil, nil, nil, nil ])
-      to_a(
+        [ [0], [0], (0..23).select { |e| e.even? }, nil, nil, nil, nil ])
+      a_eq(
         '0 7-23/2 * * *',
-        [ [0], [0], (7..23).select { |e| e.odd? }, nil, nil, nil, nil, nil ])
-      to_a(
+        [ [0], [0], (7..23).select { |e| e.odd? }, nil, nil, nil, nil ])
+      a_eq(
         '*/10 * * * *',
-        [ [0], [0, 10, 20, 30, 40, 50], nil, nil, nil, nil, nil, nil ])
+        [ [0], [0, 10, 20, 30, 40, 50], nil, nil, nil, nil, nil ])
 
       # fighting https://github.com/jmettraux/rufus-scheduler/issues/65
       #
-      to_a(
+      a_eq(
         '*/10 * * * * Europe/Berlin',
         [ [0], [ 0, 10, 20, 30, 40, 50], nil, nil, nil, nil, nil, 'Europe/Berlin' ])
     end
@@ -161,9 +166,9 @@ describe Rufus::Scheduler::CronLine do
 
       # fighting https://github.com/jmettraux/rufus-scheduler/issues/65
 
-      to_a(
+      a_eq(
         '/10 * * * *',
-        [ [0], [ 0, 10, 20, 30, 40, 50], nil, nil, nil, nil, nil, nil ])
+        [ [0], [ 0, 10, 20, 30, 40, 50], nil, nil, nil, nil, nil, ])
     end
 
     it 'rejects / for days (every other wednesday)' do
@@ -182,30 +187,20 @@ describe Rufus::Scheduler::CronLine do
 
     it 'accepts items with initial 0' do
 
-      to_a(
-        '09 * * * *', [ [0], [9], nil, nil, nil, nil, nil, nil ])
-      to_a(
-        '09-12 * * * *', [ [0], [9, 10, 11, 12], nil, nil, nil, nil, nil, nil ])
-      to_a(
-        '07-08 * * * *', [ [0], [7, 8], nil, nil, nil, nil, nil, nil ])
-      to_a(
-        '* */08 * * *', [ [0], nil, [0, 8, 16], nil, nil, nil, nil, nil ])
-      to_a(
-        '* */07 * * *', [ [0], nil, [0, 7, 14, 21], nil, nil, nil, nil, nil ])
-      to_a(
-        '* 01-09/04 * * *', [ [0], nil, [1, 5, 9], nil, nil, nil, nil, nil ])
-      to_a(
-        '* * * * 06', [ [0], nil, nil, nil, nil, [6], nil, nil ])
+      a_eq('09 * * * *', [ [0], [9], nil, nil, nil, nil, nil ])
+      a_eq('09-12 * * * *', [ [0], [9, 10, 11, 12], nil, nil, nil, nil, nil ])
+      a_eq('07-08 * * * *', [ [0], [7, 8], nil, nil, nil, nil, nil ])
+      a_eq('* */08 * * *', [ [0], nil, [0, 8, 16], nil, nil, nil, nil ])
+      a_eq('* */07 * * *', [ [0], nil, [0, 7, 14, 21], nil, nil, nil, nil ])
+      a_eq('* 01-09/04 * * *', [ [0], nil, [1, 5, 9], nil, nil, nil, nil ])
+      a_eq('* * * * 06', [ [0], nil, nil, nil, nil, [6], nil, ])
     end
 
     it 'interprets cron strings with L correctly' do
 
-      to_a(
-        '* * L * *', [[0], nil, nil, ['L'], nil, nil, nil, nil ])
-      to_a(
-        '* * 2-5,L * *', [[0], nil, nil, [2,3,4,5,'L'], nil, nil, nil, nil ])
-      to_a(
-        '* * */8,L * *', [[0], nil, nil, [1,9,17,25,'L'], nil, nil, nil, nil ])
+      a_eq('* * L * *', [[0], nil, nil, ['L'], nil, nil, nil ])
+      a_eq('* * 2-5,L * *', [[0], nil, nil, [2,3,4,5,'L'], nil, nil, nil ])
+      a_eq('* * */8,L * *', [[0], nil, nil, [1,9,17,25,'L'], nil, nil, nil ])
     end
 
     # issue https://github.com/jmettraux/rufus-scheduler/issues/202
@@ -246,26 +241,18 @@ describe Rufus::Scheduler::CronLine do
 
     it 'accepts L for day-of-week' do
 
-      expect(
-        cla '* * * * 5L'
-      ).to eq(
-        [ [ 0 ], nil, nil, nil, nil, nil, [ '5#-1' ], nil ]
-      )
-      expect(
-        cla '* * * * FRIL'
-      ).to eq(
-        [ [ 0 ], nil, nil, nil, nil, nil, [ '5#-1' ], nil ]
-      )
+      a_eq '* * * * 5L', [ [ 0 ], nil, nil, nil, nil, nil, [ '5#-1' ] ]
+      a_eq '* * * * FRIL', [ [ 0 ], nil, nil, nil, nil, nil, [ '5#-1' ] ]
     end
 
     it 'accepts negative days' do
 
-      to_a('* * 8,-8 * *', [[0], nil, nil, [-8,8], nil, nil, nil, nil ])
+      a_eq('* * 8,-8 * *', [[0], nil, nil, [-8,8], nil, nil, nil ])
     end
 
     it 'accepts negative day ranges' do
 
-      to_a('* * -10--8 * *', [[0], nil, nil, [-10,-9,-8], nil, nil, nil, nil ])
+      a_eq('* * -10--8 * *', [[0], nil, nil, [-10,-9,-8], nil, nil, nil ])
     end
 
     it 'rejects day descending ranges' do
@@ -303,32 +290,27 @@ describe Rufus::Scheduler::CronLine do
 
     it 'sorts seconds' do
 
-      to_a(
-        '23,30,10 * * * * *', [ [10,23,30], nil, nil, nil, nil, nil, nil, nil ])
+      a_eq('23,30,10 * * * * *', [ [10,23,30], nil, nil, nil, nil, nil, nil ])
     end
 
     it 'sorts minutes' do
 
-      to_a(
-        '23,30,10 * * * * ', [ [0], [10,23,30], nil, nil, nil, nil, nil, nil ])
+      a_eq('23,30,10 * * * * ', [ [0], [10,23,30], nil, nil, nil, nil, nil ])
     end
 
     it 'sorts days' do
 
-      to_a(
-        '* * 14,7 * * ', [ [0], nil, nil, [7, 14], nil, nil, nil, nil ])
+      a_eq('* * 14,7 * * ', [ [0], nil, nil, [7, 14], nil, nil, nil ])
     end
 
     it 'sorts months' do
 
-      to_a(
-        '* * * 11,3,4 * ', [ [0], nil, nil, nil, [3,4,11], nil, nil, nil ])
+      a_eq('* * * 11,3,4 * ', [ [0], nil, nil, nil, [3,4,11], nil, nil ])
     end
 
     it 'sorts days of week' do
 
-      to_a(
-        '* * * * Sun,Fri,2 ', [ [0], nil, nil, nil, nil, [0, 2, 5], nil, nil ])
+      a_eq('* * * * Sun,Fri,2 ', [ [0], nil, nil, nil, nil, [0,2,5], nil ])
     end
   end
 
